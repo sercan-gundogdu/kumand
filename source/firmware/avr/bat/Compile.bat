0ea182e23f3654454967be58da3986c1013661b9
sercan-gundogdu <sercan.gundogdu@yandex.com>
2019-06-10 13:56:04 +0300

    rename avr firmware folder

--- source/firmware/avr/bat/Compile.bat
+++
@@ -1,5 +0,0 @@
-REM Batch calling assembler
-:cmp
-avrasm2.exe -fI kumand.asm -e kumand.efile -m kumand.mfile -l kumand.lfile
-PAUSE
-GOTO cmp
--- source/firmware/avr/bat/Upload.bat
+++
@@ -1,3 +0,0 @@
-REM Batch calling assembler
-avrdude -c usbtiny -p m328p -U flash:w:kumand.hex -U eeprom:w:kumand.efile
-PAUSE
